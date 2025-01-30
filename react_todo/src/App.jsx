c87842e1b59914f24c9480dd0c38efcb30b5ab0a
ngsw <ngsw993@gmail.com>
2025-01-30 19:52:23 +0900

    編集機能追加

--- react_todo/src/App.jsx
+++ react_todo/src/App.jsx
@@ -3,9 +3,12 @@ import './App.css'
 
 const App = () => {
 
-  const [jobs, setJobs] = useState([])
+  const [jobs, setJobs] = useState([{ id: 1, top: '買い物に行く'}])
   const [jobTop, setJobTop] = useState('')
   const [jobId, setJobId] = useState(jobs.length + 1)
+  const [isEditable, setIsEditable] = useState(false)
+  const [editId, setEditId] = useState('')
+  const [newTop, setNewTop] = useState('')
 
   const handleInputChange = (e) => {
     setJobTop(e.target.value)
@@ -21,8 +24,45 @@ const App = () => {
     setJobs(jobs.filter((job) => job !== targetJob))
   }
 
+  const handleOpenEditForm = (job) => {
+    setIsEditable(true)
+    setEditId(job.id)
+    setNewTop(job.top)
+  }
+
+  const handleEditFormChange = (e) => {
+    setNewTop(e.target.value)
+  }
+
+  const handleCloseEditForm = () => {
+    setIsEditable(false)
+    setEditId('')
+  }
+
+  const handleEditTodo = () => {
+    const newArray = jobs.map((job) =>
+      job.id === editId ? { ...job, top: newTop } : job
+    )
+    setJobs(newArray)
+    setEditId('')
+    setNewTop('')
+    handleCloseEditForm()
+  }
+
   return (
     <>
+      {isEditable ? (
+        <div>
+          <input
+            type="text"
+            label="新しいタイトル"
+            value={newTop}
+            onChange={handleEditFormChange}
+          />
+        <button onClick={handleEditTodo}>編集を保存</button>
+        <button onClick={handleCloseEditForm}>キャンセル</button>
+      </div>
+      ) : (
     <div>
       <input
         type='text' 
@@ -32,12 +72,13 @@ const App = () => {
         />
       <button onClick={handleAddJob}>Create</button>
     </div>
-
+      )}
     <div>
       <ul>
         {jobs.map((job) => (
           <li key={job.id}>
             <span>{job.top}</span>
+            <button onClick={() => handleOpenEditForm(job)}>edit</button>
             <button onClick={() => handleDeleteJob(job)}>Delete</button>
           </li>
         ))}
